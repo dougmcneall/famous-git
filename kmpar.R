a3690946b8b12373659e60e36ffe701cd821fbf4
dougmcneall <dougmcneall@gmail.com>
2016-11-25 15:51:32 +0000

    basic working parallelised pc emulation
    basic working parallelised pc emulation

--- kmpar.R
+++ kmpar.R
@@ -91,7 +91,6 @@ library(RColorBrewer)
 library(fields)
 
 rb <- brewer.pal(11, "RdBu")
-ryg <- brewer.pal(11, "RdYlGn")
 bg <- brewer.pal(9, "BuGn")
 yg <- brewer.pal(9, "YlGn")
 
@@ -298,10 +297,240 @@ map("world2", ylim=c(-90,90), xlim = c(0,360), add = TRUE)
 # How do errors change as the number of PCs goes up?
 # How do things change as 
 
+source("https://raw.githubusercontent.com/dougmcneall/packages-git/master/emtools.R")
+source("https://raw.githubusercontent.com/dougmcneall/packages-git/master/imptools.R")
+source("https://raw.githubusercontent.com/dougmcneall/packages-git/master/vistools.R")
+
 # Note, get the raw version by clicking the 'download' version on github, and copying the path.
 gitdaturl = 'https://github.com/dougmcneall/famous-git/raw/master/famous_forest_fraction.RData'
 download.file(gitdaturl, 'myfile')
 load('myfile')
+
+yg <- brewer.pal(9, "YlGn")
+
+na.map = is.na(bl.frac.ens)
+bl.frac.nona = bl.frac.ens
+bl.frac.nona[na.map] = 0
+
+X = full_frac[,2:8]
+X.norm = normalize(X)
+X.stan.norm = normalize(matrix(X.standard, nrow = 1), wrt=X)
+
+bl.frac.pc = km.pc(Y=bl.frac.nona, X=X.norm, newdata=X.stan.norm, num.pc=3)
+
+image.plot(longs, rev(lats), 
+           remap.famous(bl.frac.pc$tens,longs, lats),
+           col=yg)
+map("world2", ylim=c(-90,90), xlim = c(0,360), add = TRUE)
+
+# What kind of error can we expect from cross validation using km.pc?
+bl.cv.out = matrix(NA, nrow = nrow(bl.frac.nona), ncol = ncol(bl.frac.nona))
+
+for(i in 1:nrow(X)){
+  X.norm.trunc = X.norm[-i, ]
+  Y.trunc = bl.frac.nona[-i, ]
+  X.new = X.norm[i, ]
+  bl.frac.pc = km.pc(Y=Y.trunc, X=X.norm.trunc, newdata=X.new, num.pc=6)
+  bl.cv.out[i, ] = bl.frac.pc$tens 
+}
+
+image.plot(longs, rev(lats), 
+           remap.famous(bl.cv.out[1, ],longs, lats),
+           col=yg)
+map("world2", ylim=c(-90,90), xlim = c(0,360), add = TRUE)
+
+cv.err = bl.cv.out - bl.frac.ens
+
+hist(c(cv.err))
+mean(abs(c(cv.err)), na.rm=TRUE)
+
+mae = function(x) {mean(abs(x), na.rm = TRUE)}
+
+cv.err.mae.map = apply(cv.err, 2, mae)
+image.plot(longs, rev(lats), 
+           remap.famous(cv.err.mae.map,longs, lats),
+           col=yg)
+map("world2", ylim=c(-90,90), xlim = c(0,360), add = TRUE)
+
+#
+plot(c(bl.frac.nona), c(bl.cv.out))
+abline(0,1, col = 'white')
+
+
+library(parallel)
+library(DiceKriging)
+
+remap.famous = function(dat,longs,lats, shift = FALSE){
+  # reshape a map in vector form so that image() like functions
+  # will plot it correctly
+  mat = matrix(dat, nrow = length(longs), ncol = length(lats))[ ,length(lats):1]
+  if(shift){
+    block1.ix = which(longs < shift)
+    block2.ix = which(longs > shift)
+    mat.shift = rbind(mat[ block2.ix, ], mat[block1.ix, ]) 
+    out = mat.shift
+  }
+  else{
+    out = mat
+  }
+  out
+}
+
+emlist = function(X, Y){
+  # Create a list of objects to be emulated
+  # X             design matrix with (nrow) instances of (ncol) inputs
+  # Y             matrix of outputs, with one row
+  #               for each row of X
+  
+  d = ncol(Y)
+  em.list = vector(mode='list', length=d)
+  
+  for(i in 1:d){
+    em.obj = NULL
+    em.obj$X = X
+    em.obj$y = Y[, i]
+    em.list[[i]] = em.obj
+  }
+  em.list
+}
+
+# !N.B. need to check that we're passing the formula in correctly.
+km.wrap = function(form, em, ...){
+  # wrapper function to fit km model to list em.
+  # each list element in em contains a design matrix X 
+  # and an output vector y
+  out = NA
+  fit = try(km(form, design=em$X, response=em$y, control=list(trace=FALSE), ...), silent=TRUE)
+  #if(class(fit) == "try-error"){
+  #  out = NA
+  #}
+  #else{
+  #  out = fit 
+  #}
+  out = fit
+  out
+}
+
+# Functions for dimension reduced emulation
+pc.project = function(pca,scores.em,Z.em,scale){
+  # project principal components
+  num.pc <- dim(scores.em)[2]
+  if (scale){
+    anom = ((pca$rotation[ ,1:num.pc] %*% t(scores.em))*pca$scale)
+    anom.sd = ((pca$rotation[ ,1:num.pc] %*% t(Z.em))*pca$scale)          
+  }
+  else {
+    anom = pca$rotation[ ,1:num.pc] %*% t(scores.em)
+    anom.sd = pca$rotation[ ,1:num.pc] %*% t(Z.em)   
+  }
+  tens = t(anom + pca$center)
+  return(list(tens = tens, anom.sd = anom.sd))
+}
+
+# need to sort this so that it gives out more of the dicekriging output
+
+direct.pred = function(form,X,Y,Xnew,...){
+  # Directly applies km in parallel to predict each column of an ensemble
+  ens.list = emlist(X=X, Y=Y)
+  km.list = mclapply(ens.list,FUN=km.wrap, form = form)
+  pred.list = mclapply(km.list,
+                       FUN = predict,
+                       newdata = as.matrix(Xnew, nrow =1),
+                       type = 'UK')
+  out.mean = sapply(pred.list, function(x) x$mean )
+  out.sd = sapply(pred.list, function(x) x$sd )
+  
+  return(list(mean=out.mean, sd=out.sd))
+}
+
+
+# OK, time to fix km.pc so that you can pass in a formula and use
+# parallelisation
+
+km.pc = function(Y, X, newdata, num.pc, scale = FALSE, center = TRUE, type = "UK", ...){
+  # Base function for emulation of high dimensional data
+  # with PCA and Gaussian Process emulator
+  
+  if (class(Y)!= 'prcomp'){
+    pca = prcomp(Y,scale=scale, center=center)
+  }
+  else{
+    pca = Y
+  }
+  if(is.matrix(newdata)!= TRUE){
+    print('matrixifying newdata')
+    newdata = matrix(newdata,nrow=1) 
+  }
+  scores.em = matrix(nrow=dim(newdata)[1],ncol=num.pc)
+  Z.em = matrix(nrow=dim(newdata)[1],ncol=num.pc)
+  
+  for (i in 1:num.pc){
+    # build the GP model
+    fit = km(design=X, response=pca$x[,i])
+    pred = predict(fit, newdata=newdata, type = type, ...)
+    scores.em[ ,i] = pred$mean
+    Z.em[ ,i] = pred$sd
+  }
+  proj = pc.project(pca, scores.em, Z.em, scale)
+  return(list(tens=proj$tens, scores.em=scores.em, Z.em=Z.em, anom.sd=proj$anom.sd))
+}
+
+
+kmpar.pc = function(Y, X, newdata, num.pc, scale = FALSE, center = TRUE, type = "UK", ...){
+  # Base function for emulation of high dimensional data
+  # with PCA and Gaussian Process emulator
+  
+  if (class(Y)!= 'prcomp'){
+    pca = prcomp(Y,scale=scale, center=center)
+  }
+  else{
+    pca = Y
+  }
+  if(is.matrix(newdata)!= TRUE){
+    print('matrixifying newdata')
+    newdata = matrix(newdata,nrow=1) 
+  }
+  scores.em = matrix(nrow=dim(newdata)[1],ncol=num.pc)
+  Z.em = matrix(nrow=dim(newdata)[1],ncol=num.pc)
+  
+  # replace loop with direct.pred
+  pred = direct.pred(form=~., X=X, Y=pca$x[,1:num.pc], Xnew=newdata )
+  
+  scores.em[1,] = pred$mean
+  Z.em[1,] = pred$sd
+  # # loop
+  # for (i in 1:num.pc){
+  #   # build the GP model
+  #   fit = km(design=X, response=pca$x[,i])
+  #   pred = predict(fit, newdata=newdata, type = type, ...)
+  #   scores.em[ ,i] = pred$mean
+  #   Z.em[ ,i] = pred$sd
+  # }
+  
+  proj = pc.project(pca, scores.em, Z.em, scale)
+  return(list(tens=proj$tens, scores.em=scores.em, Z.em=Z.em, anom.sd=proj$anom.sd))
+}
+
+bl.frac.pc = km.pc(Y=bl.frac.nona, X=X.norm, newdata=X.stan.norm, num.pc=3)
+
+bl.frac.pc.par = kmpar.pc(Y=bl.frac.nona, X=X.norm, newdata=X.stan.norm, num.pc=3)
+
+image.plot(longs, rev(lats), 
+           remap.famous(bl.frac.pc$tens,longs, lats),
+           col=yg)
+map("world2", ylim=c(-90,90), xlim = c(0,360), add = TRUE)
+
+image.plot(longs, rev(lats), 
+           remap.famous(bl.frac.pc.par$tens,longs, lats),
+           col=yg)
+map("world2", ylim=c(-90,90), xlim = c(0,360), add = TRUE)
+
+
+
+
+
+
+
 
 
 
